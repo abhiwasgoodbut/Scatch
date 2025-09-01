05d8a518d88a5ca2355a44d637669df61ca903ad
Abhinay Singh <sabhinay427@gmail.com>
2025-09-01 11:21:07 +0530

    add to cart finally added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const isLoggedIn = require("../middleware/isLoggedIn");
 const productModel = require("../models/product-model");
 const userModel = require("../models/user-model");
+const { useReducer } = require("react");
 const router = express.Router();
 
 router.get("/", function (req, res) {
   let error = req.flash("error");
   res.render("index", { error, loggedin: false });
 });
+
+router.get("/cart",isLoggedIn,async function(req,res){
+  let user = await userModel
+  .findOne({email: req.user.email})
+  .populate("cart");
+  // console.log(user.cart);
+  
+  res.render("cart",{user});
+})
 
 router.get("/shop", isLoggedIn, async function (req, res) {
   let products = await productModel.find();
